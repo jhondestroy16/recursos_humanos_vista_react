649c175c88131977dd2d61966696c8d13f374484
jhondestroy16 <jsvalenciag@ut.edu.co>
2025-03-11 20:21:14 -0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import ListadoEmpleados from './empleados/ListadoEmpleados';
 import Inicio from './inicio/Inicio';
+import Login from './login/Login';
 import AgregarEmpleado from './empleados/AgregarEmpleado';
 import EditarEmpleado from './empleados/EditarEmpleado';
 import ListarDepartamentos from './departamentos/listarDepartamentos';
@@ -9,6 +10,7 @@ import CrearDepartamento from './departamentos/CrearDepartamento';
 import EditarDepartamento from './departamentos/EditarDepartamento';
 import Navegacion from './plantilla/Navegacion';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute'; // Ajusta la ruta según tu estructura
 
 const App = () => {
     return (
@@ -16,13 +18,57 @@ const App = () => {
             <BrowserRouter>
                 <Navegacion />
                 <Routes>
-                    <Route exact path='/' element={<Inicio />}/>
-                    <Route exact path='/empleados' element={<ListadoEmpleados/>}/>
-                    <Route exact path='/departamentos' element={<ListarDepartamentos />}/>
-                    <Route exact path='/departamentos/crear' element={<CrearDepartamento />} />
-                    <Route exact path='/departamentos/editar/:id' element={<EditarDepartamento />} />
-                    <Route exact path='/empleados/agregar' element={<AgregarEmpleado />} />
-                    <Route exact path='/empleados/editar/:id' element={<EditarEmpleado />} />
+                    <Route exact path='/' element={<Inicio />} />
+                    <Route exact path='/login' element={<Login />} />
+                    {/* Rutas protegidas usando PrivateRoute */}
+                    <Route
+                        path='/empleados'
+                        element={
+                            <PrivateRoute>
+                                <ListadoEmpleados />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        path='/departamentos'
+                        element={
+                            <PrivateRoute>
+                                <ListarDepartamentos />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        path='/departamentos/crear'
+                        element={
+                            <PrivateRoute>
+                                <CrearDepartamento />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        path='/departamentos/editar/:id'
+                        element={
+                            <PrivateRoute>
+                                <EditarDepartamento />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        path='/empleados/agregar'
+                        element={
+                            <PrivateRoute>
+                                <AgregarEmpleado />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        path='/empleados/editar/:id'
+                        element={
+                            <PrivateRoute>
+                                <EditarEmpleado />
+                            </PrivateRoute>
+                        }
+                    />
                 </Routes>
             </BrowserRouter>
         </div>
